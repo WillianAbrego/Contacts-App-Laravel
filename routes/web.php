df97dc91f090e48267a14e788d1004846869ae69
WillianAbrego <willianantonioabrego@gmail.com>
2023-01-01 15:17:47 -0600

    stripe App Contact

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => auth()->check() ? redirect('/home') : view('welcome'));
 
 Auth::routes();
+
+Route::get('/checkout', function (Request $request) {
+    return $request->user()
+        ->newSubscription('default', config('stripe.price_id'))
+        ->checkout();
+});
+
+Route::get('/billing-portal', function (Request $request) {
+    return $request->user()->redirectToBillingPortal();
+});
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
